71e36c21c83d818844d5cf887b6f7d2179e3d6c7
karolineterra <terra.karoline1@gmail.com>
2023-09-17 12:11:57 -0300

    funcoes

--- build/index.js
+++ build/index.js
@@ -2,5 +2,8 @@
 function addNumber(x, y) {
     return x + y;
 }
+function addToHello(name) {
+    return `Hello ${name}`;
+}
 let soma = addNumber(4, 7);
-console.log(soma);
+console.log(addToHello("Karoline"));
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ function addToHello(name: string) {
     return `Hello ${name}`;
 }
 
+function CallToPhone(phone: number | string): number | string {
+    return phone
+} 
+
+async function getDatabase(id: number): Promise<number | string> {
+    return "karol";
+}
+
 let soma: number = addNumber(4, 7);
 
-console.log(soma);
+console.log(CallToPhone("1113311"));
+console.log(CallToPhone(12343423));
+
+
